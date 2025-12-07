d9e8da0192a784c067d70f11c7082f3812450911
yoni-haber <133662871+yoni-haber@users.noreply.github.com>
2025-12-07 14:16:26 +0000

    Update home route feature test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,6 +13,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get(route('home'));
 
-        $response->assertStatus(200);
+        $response->assertRedirect(route('login'));
     }
 }
